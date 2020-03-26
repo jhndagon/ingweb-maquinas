e19debf0a235285e886e982aa7c617a752dc5349
John David Gonzalez <jhndagon@gmail.com>
2020-03-26 13:44:04 -0500

    Variables de entorno configuracion

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,6 @@ insert into roles(nombre) values ("ROLE_USER");
 
 insert into usuarios(nombre, usuario, contrasenia, id_rol) values("John","john", "$2a$10$hZDgewXA2XS4IXsM8cfep.RymaBXzGipYncB4NBCC853YONAjYCMy", 1);
 insert into usuarios(nombre, usuario, contrasenia, id_rol) values("David","david", "$2a$10$7e6XaWDzJQEuJB93yv/QlOoKI6lCA04SkgMQhHAWt1ih7cT03.Plu", 2);
+insert into usuarios(nombre, usuario, contrasenia, id_rol) values("Mateo","mateo", "$2a$10$hZDgewXA2XS4IXsM8cfep.RymaBXzGipYncB4NBCC853YONAjYCMy", 1);
+insert into usuarios(nombre, usuario, contrasenia, id_rol) values("Mateo","mateo123", "$2a$10$hZDgewXA2XS4IXsM8cfep.RymaBXzGipYncB4NBCC853YONAjYCMy", 1);
+insert into usuarios(nombre, usuario, contrasenia, id_rol) values("Paula","paula", "$2a$10$hZDgewXA2XS4IXsM8cfep.RymaBXzGipYncB4NBCC853YONAjYCMy", 1);
