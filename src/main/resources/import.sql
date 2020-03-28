4ebaaa2342b2a80ece08ef153c2d604ae75af826
John David Gonzalez <jhndagon@gmail.com>
2020-03-27 21:57:17 -0500

    Aniadido los cors al back

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
 insert into roles(nombre) values ("ROLE_ADMIN");
-insert into roles(nombre) values ("ROLE_USER");
+insert into roles(nombre) values ("ROLE_EMPLEADO");
+insert into roles(nombre) values ("ROLE_ADMIN_PUNTO");
+insert into roles(nombre) values ("ROLE_RECURSO_HUMANO");
 
 insert into usuarios(nombre, usuario, contrasenia, id_rol) values("John","john", "$2a$10$hZDgewXA2XS4IXsM8cfep.RymaBXzGipYncB4NBCC853YONAjYCMy", 1);
 insert into usuarios(nombre, usuario, contrasenia, id_rol) values("David","david", "$2a$10$7e6XaWDzJQEuJB93yv/QlOoKI6lCA04SkgMQhHAWt1ih7cT03.Plu", 2);
