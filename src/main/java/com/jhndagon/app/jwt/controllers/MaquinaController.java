44cfce224d1f80c1869ab4859698f17621dec01a
cgc9 <caro.gc197@gmail.com>
2020-04-26 07:05:50 -0500

    Manejo de excepciones punto de venta y maquinas

--- src/main/java/com/jhndagon/app/jwt/controllers/MaquinaController.java
+++ src/main/java/com/jhndagon/app/jwt/controllers/MaquinaController.java
@@ -1,14 +1,19 @@
 package com.jhndagon.app.jwt.controllers;
 
 import com.jhndagon.app.jwt.models.Maquina;
+import com.jhndagon.app.jwt.models.PuntoDeVenta;
 import com.jhndagon.app.jwt.services.IMaquinaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/maquinas")
@@ -19,16 +24,47 @@ public class MaquinaController {
 
     @Secured({"ROLE_ADMIN"})
     @PostMapping("/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Maquina crearMaquina(@RequestBody Maquina maquina) {
-        return maquinaService.createMaquina(maquina);
+    public ResponseEntity<?> crearMaquina(@RequestBody Maquina maquina) {
+        Maquina maquinaNew=null;
+        Map<String, Object> response = new HashMap<>();
+
+        try{
+            maquinaNew= maquinaService.createMaquina(maquina);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","La maquina ha sido creada");
+        response.put("Maquina",maquinaNew);
+
+        return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
+
     }
 
     @Secured({"ROLE_ADMIN_PUNTO", "ROLE_ADMIN"})
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Maquina maquina(@PathVariable Long id) {
-        return maquinaService.findById(id);
+    public ResponseEntity<?> maquina(@PathVariable Long id) {
+        Maquina maquina=null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            maquina = maquinaService.findById(id);
+
+        }catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(maquina==null){
+            response.put("mensaje", "La maquina con id: ".concat(id.toString().concat(", no existe en la base de datos.")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Maquina>(maquina, HttpStatus.OK);
+
     }
 
     @Secured({"ROLE_ADMIN_PUNTO", "ROLE_ADMIN"})
@@ -44,15 +80,50 @@ public class MaquinaController {
     @Secured({"ROLE_ADMIN"})
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Maquina updateMaquina(@RequestBody Maquina maquina,@PathVariable Long id){
-        return maquinaService.updateMaquina(maquina,id);
+    public ResponseEntity<?> updateMaquina(@RequestBody Maquina maquina,@PathVariable Long id){
+
+        Maquina maquinaActual= maquinaService.findById(id);
+        Maquina maquinaUp =null;
+        Map<String, Object> response = new HashMap<>();
+
+        if(maquinaActual==null){
+            response.put("mensaje", "La maquina con id: ".concat(id.toString().concat(", no existe en la base de datos.")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            maquinaUp=maquinaService.updateMaquina(maquina,id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","La maquina ha sido actualizada");
+        response.put("Maquina",maquinaUp);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+
+
     }
 
     @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void eliminarMaquina(@PathVariable Long id) {
-        maquinaService.deleteMaquina(id);
+    public ResponseEntity<?> eliminarMaquina(@PathVariable Long id) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            maquinaService.deleteMaquina(id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","La maquina ha sido eliminada");
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 
 
--- src/main/java/com/jhndagon/app/jwt/controllers/PuntoDeVentaController.java
+++ src/main/java/com/jhndagon/app/jwt/controllers/PuntoDeVentaController.java
@@ -1,58 +1,124 @@
 package com.jhndagon.app.jwt.controllers;
 
 import com.jhndagon.app.jwt.models.PuntoDeVenta;
+import com.jhndagon.app.jwt.models.Usuario;
 import com.jhndagon.app.jwt.services.IPuntoDeVentaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/puntos-de-ventas")
 public class PuntoDeVentaController {
     @Autowired
     private IPuntoDeVentaService puntoDeVentaService;
-    
+
     @Secured({"ROLE_ADMIN"})
     @PostMapping("/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public PuntoDeVenta crearPuntoDeVenta(@RequestBody PuntoDeVenta puntoDeVenta ){
-        return puntoDeVentaService.createPuntoDeVenta(puntoDeVenta);
+    public ResponseEntity<?> crearPuntoDeVenta(@RequestBody PuntoDeVenta puntoDeVenta ){
+        PuntoDeVenta puntoDeVentaNew=null;
+        Map<String, Object> response = new HashMap<>();
+
+        try{
+            puntoDeVentaNew=puntoDeVentaService.createPuntoDeVenta(puntoDeVenta);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El punto de venta ha sido creado");
+        response.put("Punto de venta",puntoDeVentaNew);
+
+
+        return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
 
     }
-  
+
     @Secured({"ROLE_RECURSO_HUMANO", "ROLE_ADMIN"})
     @GetMapping("/")
-    @ResponseStatus(HttpStatus.OK)
     public Page<PuntoDeVenta> puntosDeVenta(@RequestParam(name="page" ,defaultValue = "0") int page,
                                             @RequestParam(defaultValue = "3") @Max(value = 10) int size){
         Page<PuntoDeVenta> puntosDeVenta=puntoDeVentaService.findAllPuntosDeVenta(page,size);
         return puntosDeVenta;
 
     }
-    
+
     @Secured({"ROLE_RECURSO_HUMANO", "ROLE_ADMIN"})
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public PuntoDeVenta puntoDeVenta(@PathVariable Long id){
-        return puntoDeVentaService.findById(id);
+    public ResponseEntity<?>puntoDeVenta(@PathVariable Long id){
+        PuntoDeVenta puntoDeVenta=null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            puntoDeVenta=puntoDeVentaService.findById(id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(puntoDeVenta==null){
+            response.put("mensaje", "El punto de venta con id: ".concat(id.toString().concat(", no existe en la base de datos.")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<PuntoDeVenta>(puntoDeVenta, HttpStatus.OK);
     }
     
     @Secured({"ROLE_ADMIN"})
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public PuntoDeVenta updatePuntoDeVenta(@RequestBody PuntoDeVenta puntoDeVenta,@PathVariable Long id){
-        return puntoDeVentaService.updatePuntoDeVenta(puntoDeVenta,id);
+    public ResponseEntity<?> updatePuntoDeVenta(@RequestBody PuntoDeVenta puntoDeVenta,@PathVariable Long id){
+
+        PuntoDeVenta puntoDeVentaActual= puntoDeVentaService.findById(id);
+        PuntoDeVenta puntoDeVentaUp =null;
+        Map<String, Object> response = new HashMap<>();
+
+        if(puntoDeVentaActual==null){
+            response.put("mensaje", "El punto de venta con id: ".concat(id.toString().concat(", no existe en la base de datos.")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            puntoDeVentaUp=puntoDeVentaService.updatePuntoDeVenta(puntoDeVenta,id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","El punto de venta ha sido actualizado");
+        response.put("Punto de venta",puntoDeVentaUp);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
     
     @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void eliminarPuntoDeVenta(@PathVariable Long id){
-        puntoDeVentaService.deletePuntoDeVenta(id);
+    public ResponseEntity<?> eliminarPuntoDeVenta(@PathVariable Long id){
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            puntoDeVentaService.deletePuntoDeVenta(id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El punto de venta ha sido eliminado");
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 
     }
 
