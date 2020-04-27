e98620ceaee21f37f0b7064424d0594232c838fe
Luis Ernesto Monsalve Arango <ernesto.monsalve@udea.edu.co>
2020-04-27 17:33:24 -0500

    Manejo de errores en proveedores

--- src/main/java/com/jhndagon/app/jwt/controllers/ProveedorController.java
+++ src/main/java/com/jhndagon/app/jwt/controllers/ProveedorController.java
@@ -3,11 +3,16 @@ package com.jhndagon.app.jwt.controllers;
 import com.jhndagon.app.jwt.models.Proveedor;
 import com.jhndagon.app.jwt.services.IProveedorService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.constraints.Max;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,20 +41,71 @@ public class ProveedorController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Proveedor Proveedor(@PathVariable Long id) {
-        return proveedorService.findProveedorById(id);
+    public ResponseEntity<?> Proveedor(@PathVariable Long id) {
+
+		Proveedor proveedor = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			proveedorService.findProveedorById(id);
+		}
+		catch (DataAccessException e){
+			response.put("mensaje", "Error al acceder a la base de datos"); 
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		}
+		if(proveedor == null) {
+			response.put("mensaje", "El proveedor con id: ".concat(id.toString().concat(", no existe en la base de datos.")));	
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Proveedor>(proveedor, HttpStatus.OK);
+	
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Proveedor updateProveedor(@RequestBody Proveedor provedor,@PathVariable Long id){
-        return proveedorService.updateProveedor(provedor,id);
+    public ResponseEntity<?>  updateProveedor(@RequestBody Proveedor proveedor,@PathVariable Long id){
+
+        Proveedor proveedorActual= proveedorService.findProveedorById(id);
+        Proveedor proveedorNew = null;
+        Map<String, Object> response = new HashMap<>();
+
+        if(proveedorActual==null){
+            response.put("mensaje", "El proveedor con id: ".concat(id.toString().concat(", no existe en la base de datos.")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            proveedorNew=proveedorService.updateProveedor(proveedor,id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","El proveedor ha sido actualizado");
+        response.put("Proveedor",proveedorNew);
+        
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void eliminarProveedor(@PathVariable Long id) {
-        proveedorService.deleteProveedor(id);
+    public ResponseEntity<?> eliminarProveedor(@PathVariable Long id) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+        	proveedorService.deleteProveedor(id);
+        }
+        catch (DataAccessException e) {
+            response.put("mensaje", "Error al acceder a la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El proveedor ha sido eliminado");
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 
 
