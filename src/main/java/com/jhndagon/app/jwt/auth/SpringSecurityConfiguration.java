8c57dcf523c7a50cb93d36050a86e62b2c77599e
John David Gonzalez <jhndagon@gmail.com>
2020-05-30 22:12:59 -0500

    bcrypt fix

--- src/main/java/com/jhndagon/app/jwt/auth/SpringSecurityConfiguration.java
+++ src/main/java/com/jhndagon/app/jwt/auth/SpringSecurityConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
-@Order(1)
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
